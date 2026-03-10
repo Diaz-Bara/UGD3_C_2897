130721607ba73a295fe635d25cbffce5ffd05ca9
Diaz-Bara <diazbara3@gmail.com>
2026-03-10 16:31:55 +0700

    Bonus

--- script.js
+++ script.js
@@ -6,21 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
         let pass = document.getElementById("password").value.trim();
         let position = document.getElementById("password");
         let errorBefore = document.getElementById("msgError");
+    
         if (errorBefore) {
             errorBefore.remove();
         }
 
-        if (usn == "" || pass == "") {
+        let errorText = "";
+        if (usn === "" || pass === "") {
+            errorText = "Username atau Password tidak boleh kosong, ya!";
+        } else if (pass.length < 8) {
+            errorText = "Password minimal harus 8 karakter, brok.";
+        } else if (pass.length > 16) {
+            errorText = "Password maksimal 16 karakter nih.";
+        }
+
+        if (errorText !== "") {
             let msgError = document.createElement("p");
             msgError.id = "msgError";
-            msgError.style.color = "red";
+            msgError.style.color = "#bf1a12"; 
             msgError.style.fontSize = "12px";
-            msgError.style.marginLeft = "5px";
-            msgError.style.textAlign = "left";
-            msgError.textContent = "Username atau Password Tidak Boleh Kosong";
+            msgError.style.marginTop = "5px";
+            msgError.textContent = errorText;
             position.after(msgError);
         } else {
             window.open("https://www.instagram.com/diaz.bara?igsh=MXhoYnhwYWZjNXJ1MQ%3D%3D&utm_source=qr");
         }
-    })
-} )
+    });
+});
